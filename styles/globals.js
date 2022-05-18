3742d0e01d33d7c33cabc172845628830a97a28c
Michael Sebar Jr <msebarjr@gmail.com>
2022-05-18 00:57:46 -0400

    Setup GlobalStyles and External StyleSheet

---
+++ styles/globals.js
@@ -0,0 +1,19 @@
+const GlobalStyles = {
+    colors: {
+        primary: "rgba(80, 104, 169, 1)",
+        secondary: "rgba(134, 166, 223, 1)",
+        faded: "rgba(0, 0, 0, 0.5)",
+        completed: "rgba(14, 173, 49, 1)",
+        pending: "rgba(220, 214, 63, 1)",
+        delete: "rgba(248, 21, 21, 1)",
+    },
+    spacing: {
+        xs: 8,
+        small: 16,
+        medium: 24,
+        large: 32,
+        xl: 40,
+    },
+};
+
+export default GlobalStyles;
